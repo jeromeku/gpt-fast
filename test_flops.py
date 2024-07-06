0ae48e2d1a7bcb4028d7697e7d523d8d92fb4ee0
jeromeku <jerome.ku@gmail.com>
2024-07-06 14:11:59 +0000

    more refactoring

--- test_flops.py
+++ test_flops.py
@@ -377,6 +377,33 @@ def test_flop_counter_manager(shape, timer_cls):
 def get_leaf_nodes(count_keys, module_name):
     return [k for k in count_keys if k.endswith(module_name)]
 
+def attn_proj_data_check(model_config, batch_size, seqlen, element_size):
+    input_size = batch_size * seqlen * model_config.hidden_size * element_size 
+    weight_size = model_config.hidden_size * model_config.hidden_size * element_size 
+    output_size = batch_size * seqlen * model_config.hidden_size * element_size
+    return input_size + weight_size + output_size
+def attn_data_check(model_config, batch_size, seqlen, element_size):
+    # queries, keys, values -> factor of 3
+    input_size = (batch_size * seqlen * model_config.hidden_size * 3) * element_size
+    output_size = (batch_size * seqlen * model_config.hidden_size) * element_size
+    return input_size + output_size 
+    
+def ffn_data_check(model_config, batch_size, seqlen, element_size, module_name):
+    assert module_name in ["up_proj", "gate_proj", "down_proj"]
+
+    if module_name == "down_proj":
+        input_size = batch_size * seqlen * model_config.intermediate_size * element_size
+    else:
+        input_size = batch_size * seqlen * model_config.hidden_size * element_size
+    weight_size = model_config.hidden_size * model_config.intermediate_size * element_size 
+    if module_name == "down_proj":
+        output_size = batch_size * seqlen * model_config.hidden_size * element_size
+    else:
+        output_size = batch_size * seqlen * model_config.intermediate_size * element_size 
+
+    return input_size + weight_size + output_size
+
+
 SMALL_MODEL_CONFIG = (1, 128, 352, 4, 32000)
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available")
 @pytest.mark.parametrize("num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, vocab_size", [SMALL_MODEL_CONFIG])
@@ -395,6 +422,7 @@ def test_performance_counter(num_hidden_layers, hidden_size, intermediate_size, 
     cfg._attn_implementation = "sdpa"
     model = LlamaForCausalLM(cfg).to(dtype).to("cuda")
     model_config = model.config
+    element_size = dtype.itemsize
     
     input_ids = torch.randint(0, model_config.vocab_size, (batch_size, seqlen), device="cuda")
     with torch.no_grad():
@@ -417,11 +445,7 @@ def test_performance_counter(num_hidden_layers, hidden_size, intermediate_size, 
         assert expected_flops == summary_flops[proj_keys[0]]
         
         # Data movement check
-        element_size = dtype.itemsize
-        input_size = batch_size * seqlen * model_config.hidden_size * element_size 
-        weight_size = model_config.hidden_size * model_config.hidden_size * element_size 
-        output_size = batch_size * seqlen * model_config.hidden_size * element_size
-        expected_size = input_size + weight_size + output_size
+        expected_size = attn_proj_data_check(model_config, batch_size, seqlen, element_size)
         assert expected_size == summary_data[proj_keys[0]]
 
     # Attention
@@ -435,15 +459,11 @@ def test_performance_counter(num_hidden_layers, hidden_size, intermediate_size, 
                 assert expected_flops == count
         for op, count in data_movement.items():
             if "attention" in op.__name__:
-                # queries, keys, values
-                input_size = (batch_size * seqlen * model_config.hidden_size * 3) * element_size
-                output_size = (batch_size * seqlen * model_config.hidden_size) * element_size
-                attn_size = input_size + output_size 
                 # Check approx equal due to other small artifacts returned by sdpa.mem_efficient_attention
                 # See #https://github.com/pytorch/pytorch/blob/main/aten/src/ATen/native/transformers/cuda/attention.cu#L867
-                # print(f"{input_size} {output_size} = {attn_size} vs {count} -> {attn_size - count}")
-                #Check within 100 bytes
-                assert abs(attn_size - count) < 100
+                # Check within 100 bytes
+                expected_size = attn_data_check(model_config, batch_size, seqlen, element_size)
+                assert abs(expected_size - count) < 100
     # FFN
     for k in ["up_proj", "gate_proj", "down_proj"]:
         proj_keys = get_leaf_nodes(summary_flops.keys(), k)
@@ -452,11 +472,5 @@ def test_performance_counter(num_hidden_layers, hidden_size, intermediate_size, 
         assert expected_flops == summary_flops[proj_keys[0]]
         
         # Data movement check
-        element_size = dtype.itemsize
-        input_size = (batch_size * seqlen * model_config.hidden_size * element_size if (k == "up_proj" or k == "gate_proj") 
-                      else batch_size * seqlen * model_config.intermediate_size * element_size)
-        weight_size = model_config.hidden_size * model_config.intermediate_size * element_size 
-        output_size = (batch_size * seqlen * model_config.intermediate_size * element_size if (k == "up_proj" or k == "gate_proj") 
-                       else batch_size * seqlen * model_config.hidden_size * element_size)
-        expected_size = input_size + weight_size + output_size
+        expected_size = ffn_data_check(model_config, batch_size, seqlen, element_size, k)
         assert expected_size == summary_data[proj_keys[0]]
