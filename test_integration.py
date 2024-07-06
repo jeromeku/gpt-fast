2a841458315150c3236bd5535d562370b1926b45
jeromeku <jerome.ku@gmail.com>
2024-07-06 14:27:26 +0000

    remove spec decode

--- test_integration.py
+++ test_integration.py
@@ -138,56 +138,6 @@ def decode_n_tokens(model: Transformer, cur_token: torch.Tensor, input_pos: torc
 def model_forward(model, x, input_pos):
     return model(x, input_pos)
 
-# def speculative_decode(
-#     model: Transformer,
-#     draft_model: Transformer,
-#     cur_token: torch.Tensor,
-#     input_pos: int,
-#     speculate_k: int,
-#     **sampling_kwargs
-# ) -> torch.Tensor:
-#     # draft model inference sequentially
-#     device = cur_token.device
-#     orig_input_pos = torch.tensor([input_pos], dtype=torch.int64, device=cur_token.device)
-#     draft_tokens, draft_probs = decode_n_tokens(draft_model, cur_token.view(1, -1), orig_input_pos.clone(), speculate_k, **sampling_kwargs)
-
-#     draft_tokens = torch.cat(draft_tokens)
-#     # parallel inference on target model using draft tokens
-#     target_logits = model_forward(
-#         model,
-#         torch.cat([cur_token.view(1), draft_tokens]).view(1, -1),
-#         torch.arange(input_pos, input_pos + speculate_k + 1, device=cur_token.device)
-#     )
-#     target_probs = logits_to_probs(target_logits[0], **sampling_kwargs)
-#     draft_probs = torch.stack(draft_probs)
-#     # q: target prob, p: draft prob
-#     # q >= p: always accept draft token
-#     # q < p: q/p prob to accept draft token
-#     p = draft_probs[torch.arange(0, speculate_k, device=device), draft_tokens]
-#     q = target_probs[torch.arange(0, speculate_k, device=device), draft_tokens]
-#     accept_draft_prob = torch.minimum(torch.ones(()), q[:speculate_k]/ p)
-#     rejected_locations = (torch.rand_like(accept_draft_prob) > accept_draft_prob).nonzero()
-
-#     if rejected_locations.shape[0] == 0: # All draft tokens have been accepted
-#         accept_length = speculate_k + 1
-#         last_token = multinomial_sample_one_no_sync(target_probs[-1])
-#         # fill last token into draft model
-#         model_forward(
-#             draft_model,
-#             draft_tokens[-1].view(1, -1),
-#             orig_input_pos + speculate_k,
-#         )
-#         return torch.cat([draft_tokens, last_token])
-#     else:
-#         accept_length = rejected_locations[0].item()
-#         p = draft_probs[accept_length]
-#         q = target_probs[accept_length]
-#         new = q - p
-#         new = torch.where(new > 0, new, 0.0)
-#         new = new / new.sum()
-#         next_token = multinomial_sample_one_no_sync(new)
-#         return torch.cat([draft_tokens[:accept_length], next_token])
-
 @torch.no_grad()
 def generate(
     model: Transformer,
@@ -203,21 +153,14 @@ def generate(
     """
     Takes a conditioning sequence (prompt) as input and continues to generate as many tokens as requested.
     """
-    is_speculative = draft_model is not None
     # create an empty tensor of the expected final shape and fill in the current tokens
     T = prompt.size(0)
     T_new = T + max_new_tokens
-    if interactive:
-        max_seq_length = 350
-    else:
-        max_seq_length = min(T_new, model.config.block_size)
+    max_seq_length = min(T_new, model.config.block_size)
 
     device, dtype = prompt.device, prompt.dtype
-    max_seq_length = max_seq_length + speculate_k + 1 if is_speculative else max_seq_length
     with torch.device(device):
         model.setup_caches(max_batch_size=1, max_seq_length=max_seq_length)
-        if is_speculative and draft_model is not model:
-            draft_model.setup_caches(max_batch_size=1, max_seq_length=max_seq_length)
 
     # create an empty tensor of the expected final shape and fill in the current tokens
     empty = torch.empty(T_new, dtype=dtype, device=device)
@@ -226,32 +169,13 @@ def generate(
     input_pos = torch.arange(0, T, device=device)
 
     next_token = prefill(model, prompt.view(1, -1), input_pos, **sampling_kwargs).clone()
-    if is_speculative:
-        prefill(draft_model, prompt.view(1, -1), input_pos, **sampling_kwargs)
     seq[T] = next_token
 
     input_pos = torch.tensor([T], device=device, dtype=torch.int)
     accept_counts = [0] * (speculate_k + 1)
 
-    if is_speculative:
-        input_pos = input_pos.item()  # for speculative decoding easier to keep on host
-        while input_pos < T_new - 1:
-            cur_token = next_token.view(())
-
-            next_tokens = speculative_decode(
-                model, draft_model, cur_token, input_pos, speculate_k, **sampling_kwargs
-            )
-
-            accept_counts[len(next_tokens) - 1] += 1
-            num_added = min(T_new - input_pos - 1, len(next_tokens))
-            seq[input_pos + 1 : input_pos + num_added + 1] = next_tokens[: num_added]
-            for i in next_tokens[: num_added,]:
-                callback(i)
-            input_pos = input_pos + num_added
-            next_token = next_tokens[-1]
-    else:
-        generated_tokens, _ = decode_n_tokens(model, next_token.view(1, -1), input_pos, max_new_tokens - 1, callback=callback, **sampling_kwargs)
-        seq[T + 1:] = torch.cat(generated_tokens)
+    generated_tokens, _ = decode_n_tokens(model, next_token.view(1, -1), input_pos, max_new_tokens - 1, callback=callback, **sampling_kwargs)
+    seq[T + 1:] = torch.cat(generated_tokens)
 
     generate_stats = {
         'accept_counts': accept_counts
@@ -322,6 +246,7 @@ def main(
                                   num_active_params=num_active_params,
                                   name=model.__class__.__name__)
     
+    model_size = MODEL_CFG.model_size
     print("Transformer config: ", MODEL_CFG)
     global SOL
     SOL = SpeedOfLightStats(device_spec=DEVICE_SPEC, model_config=MODEL_CFG)
